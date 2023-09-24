b9833a24e1090c780089655613e5911479f4168d
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-09-24 15:55:59 +0300

    feat: final change media files

--- audio-player/assets/playlist/playlist.js
+++ audio-player/assets/playlist/playlist.js
@@ -1,20 +1,62 @@
 let playlist = [
     {
-        name: 'Winamp - Start',
+        name: 'Winamp - Starts',
         artist: 'Winamp',
         img: 'track-0',
         src: 'track-0',
     },
     {
-        name: 'It`s Rainig Man',
-        artist: 'Man',
+        name: 'Far From Any Road',
+        artist: 'The Handsome Family',
         img: 'track-1',
         src: 'track-1',
     },
     {
-        name: 'Crying Woman - Cry',
-        artist: 'Woman',
+        name: 'Hurt',
+        artist: 'Johnny Cash',
         img: 'track-2',
         src: 'track-2',
+    },
+    {
+        name: 'Yeah Right',
+        artist: 'Joji',
+        img: 'track-3',
+        src: 'track-3',
+    },
+    {
+        name: 'Gnossienne: No. 1',
+        artist: 'Erik Satie',
+        img: 'track-4',
+        src: 'track-4',
+    },
+    {
+        name: 'Gnossienne: No. 2',
+        artist: 'Erik Satie',
+        img: 'track-5',
+        src: 'track-5',
+    },
+    {
+        name: 'Gnossienne: No. 3',
+        artist: 'Erik Satie',
+        img: 'track-6',
+        src: 'track-6',
+    },
+    {
+        name: 'Gnossienne: No. 4',
+        artist: 'Erik Satie',
+        img: 'track-7',
+        src: 'track-7',
+    },
+    {
+        name: 'Gnossienne: No. 5',
+        artist: 'Erik Satie',
+        img: 'track-8',
+        src: 'track-8',
+    },
+    {
+        name: 'Gnossienne: No. 6',
+        artist: 'Erik Satie',
+        img: 'track-9',
+        src: 'track-9',
     },
 ];
