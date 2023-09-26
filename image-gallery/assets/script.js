2d9f39c80d97e8ea0bd4981870bf66290ff644dd
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-09-26 16:19:42 +0300

    refactor: main block

--- image-gallery/assets/script.js
+++ image-gallery/assets/script.js
@@ -1,1 +1,11 @@
 console.log('start');
+const apiKey = '3IDd65DtUkHcD7WW4Aj4kQbHDiLF6bAie9aDTHhDT1Q';
+const perPage = 15;
+let currentPage = 1;
+const getImages = async () => {
+    const url = `https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=${apiKey}`;
+    const data = await fetch(url);
+
+    console.log(data);
+}
+// getImages();
