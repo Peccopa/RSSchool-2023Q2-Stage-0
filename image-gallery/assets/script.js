7ed969535d4c3b4524e6d03bcae84fd9e616a3f4
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-09-27 10:05:24 +0300

    feat: add dinamic images

--- image-gallery/assets/script.js
+++ image-gallery/assets/script.js
@@ -1,11 +1,42 @@
-console.log('start');
+const imagesBlock = document.querySelector('.images');
+const loadMore = document.querySelector('.load-more');
+
 const apiKey = '3IDd65DtUkHcD7WW4Aj4kQbHDiLF6bAie9aDTHhDT1Q';
 const perPage = 15;
 let currentPage = 1;
-const getImages = async () => {
-    const url = `https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=${apiKey}`;
-    const data = await fetch(url);
+const url = `https://api.unsplash.com/search/photos?query=spring&per_page=${perPage}&orientation=landscape&client_id=${apiKey}`;
+let state = [];
+//test
+const urlTest = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=15`;
+//get images
+const getImages = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-    console.log(data);
-}
-// getImages();
+        if(response.ok && data.length) {
+            state = data;
+            setImages();
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+//set images
+const setImages = (images) => {
+    console.log(state);
+    imagesBlock.innerHTML += state.map(img =>
+        `<li class="card"><img src="${img.urls.regular}" alt="photo">
+            <div class="info">
+                <div class="author">
+                    <span class="material-symbols-outlined">photo_camera</span>
+                    <p>${img.user.name}</p>
+                </div>
+                <button><span class="material-symbols-outlined">download</span></button>
+            </div>
+        </li>`
+    ).join('');
+};
+
+//run
+getImages(urlTest);
