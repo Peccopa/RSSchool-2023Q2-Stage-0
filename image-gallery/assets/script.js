5137d965ecdcf7cf5a9dfef7905a8be5a0c11848
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-09-27 18:02:05 +0300

    feat: add active modal window

--- image-gallery/assets/script.js
+++ image-gallery/assets/script.js
@@ -1,24 +1,21 @@
 const imagesBlock = document.querySelector('.images');
 const loadMoreBtn = document.querySelector('.load-more');
 const searchInput = document.querySelector('.search-input');
-
+const modalBlock = document.querySelector('.modal-block');
+const modalClose = document.querySelector('.mw-close');
+const modalDownload = document.querySelector('.mw-download');
 const apiKey = '3IDd65DtUkHcD7WW4Aj4kQbHDiLF6bAie9aDTHhDT1Q';
 const perPage = 15;
 let currentPage = 1;
 let inputValue = null;
 const url = `https://api.unsplash.com/search/photos?query=pictures&page=${currentPage}&per_page=${perPage}&client_id=${apiKey}`;
-// const url = `https://api.unsplash.com/search/photos?query=spring&per_page=${perPage}&orientation=landscape&client_id=${apiKey}`;
 let state = [];
-//test
-// const urlTest = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=15`;
 //get images
 const getImages = async (url) => {
-    // currentPage++;
     loadMoreBtn.textContent = 'Loading...';
     loadMoreBtn.classList.add('disabled');
     try {
         const response = await fetch(url);
-        // console.log(response);
         const data = await response.json();
         console.log(data);
         if(response.ok && data.results.length) {
@@ -26,27 +23,25 @@ const getImages = async (url) => {
             loadMoreBtn.textContent = 'Show me more';
             loadMoreBtn.classList.remove ('disabled');
             setImages();
-            // console.log('yeap');
         }
-        // console.log('noup');
     } catch (err) {
-        console.log(err);
+        alert('Error.json');
     }
 };
 //set images
 const setImages = (images) => {
-    console.log(currentPage);
     imagesBlock.innerHTML += state.map(img =>
-        `<li class="card"><img src="${img.urls.regular}" alt="photo">
-            <div class="info">
-                <div class="author">
-                    <span class="material-symbols-outlined">photo_camera</span>
-                    <p>${img.user.name}</p>
+        `<li class="card" onclick="openModalBlock('${img.user.name}', '${img.urls.regular}')">
+            <img src="${img.urls.regular}" alt="photo">
+                <div class="info">
+                    <div class="author">
+                        <span class="material-symbols-outlined">photo_camera</span>
+                        <p>${img.user.name}</p>
+                    </div>
+                    <button onclick="download('${img.urls.raw}');event.stopPropagation();">
+                        <span class="material-symbols-outlined">download</span>
+                    </button>
                 </div>
-                <button onclick="download('${img.urls.regular}')">
-                    <span class="material-symbols-outlined">download</span>
-                </button>
-            </div>
         </li>`
     ).join('');
 };
@@ -63,55 +58,45 @@ const searchImages = (e) => {
 const loadMore = () => {
     currentPage++;
     let url = `https://api.unsplash.com/search/photos?query=${inputValue}&page=${currentPage}&per_page=${perPage}&client_id=${apiKey}`;
-
     getImages(url);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let test = 'https://images.unsplash.com/photo-1497030947858-3f40f1508e84?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MDc2OTR8MHwxfHNlYXJjaHwxMnx8cGljdHVyZXN8ZW58MHx8fHwxNjk1ODExNDk4fDA&ixlib=rb-4.0.3&q=80&w=1080';
-
-//download
-const download = async (test) => {
-        console.log(test);
-        const response = await fetch(test);
-        console.log(response);
-        const data = await response.blob();
-        console.log(data);
+// download
+const download = async (urlImg) => {
+    try {
+        const response = await fetch(urlImg);
+        const file = await response.blob();
+        if(response.ok && file.size) {
+            const a = document.createElement('a');
+            a.href = URL.createObjectURL(file);
+            a.download = new Date().getTime();
+            a.click();
+        }
+    } catch (err) {
+        alert('Error.blob');
+    }
+};
+//open modal window on click
+const openModalBlock = (name, img) => {
+    modalBlock.querySelector('img').src = img;
+    modalBlock.querySelector('p').textContent = name;
+    modalDownload.setAttribute('data-img', img);
+    modalBlock.classList.add('show');
+};
+//close modall
+const closeModalWindow = () => {
+    modalBlock.classList.remove('show');
 }
-
-download(test);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //run
 getImages(url);
 //load btns event
 loadMoreBtn.addEventListener('click', loadMore);
 searchInput.addEventListener('keyup', searchImages);
+document.addEventListener('keyup', (e) => {
+    if(e.key === 'Escape') {
+        closeModalWindow();
+    }
+});
+modalClose.addEventListener('click', closeModalWindow);
+modalDownload.addEventListener('click', (e) => {
+    download(e.target.dataset.img);
+});
