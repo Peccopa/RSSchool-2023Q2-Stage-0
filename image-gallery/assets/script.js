dc0ea96ab80ea281ba88165569f45c84a5c09dee
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-09-27 13:03:12 +0300

    feat: add search images

--- image-gallery/assets/script.js
+++ image-gallery/assets/script.js
@@ -1,23 +1,34 @@
 const imagesBlock = document.querySelector('.images');
-const loadMore = document.querySelector('.load-more');
+const loadMoreBtn = document.querySelector('.load-more');
+const searchInput = document.querySelector('.search-input');
 
 const apiKey = '3IDd65DtUkHcD7WW4Aj4kQbHDiLF6bAie9aDTHhDT1Q';
-const perPage = 15;
-let currentPage = 1;
-const url = `https://api.unsplash.com/search/photos?query=spring&per_page=${perPage}&orientation=landscape&client_id=${apiKey}`;
+const perPage = 3;
+let currentPage = 0;
+let inputValue = null;
+const url = `https://api.unsplash.com/search/photos?query=gallery&page=${currentPage}&per_page=${perPage}&client_id=${apiKey}`;
+// const url = `https://api.unsplash.com/search/photos?query=spring&per_page=${perPage}&orientation=landscape&client_id=${apiKey}`;
 let state = [];
 //test
-const urlTest = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=15`;
+// const urlTest = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=15`;
 //get images
 const getImages = async (url) => {
+    currentPage++;
+    loadMoreBtn.textContent = 'Loading...';
+    loadMoreBtn.classList.add('disabled');
     try {
         const response = await fetch(url);
+        console.log(response);
         const data = await response.json();
-
-        if(response.ok && data.length) {
-            state = data;
+        console.log(data);
+        if(response.ok && data.results.length) {
+            state = data.results;
+            loadMoreBtn.textContent = 'Show me more';
+            loadMoreBtn.classList.remove ('disabled');
             setImages();
+            console.log('yeap');
         }
+        console.log('noup');
     } catch (err) {
         console.log(err);
     }
@@ -37,6 +48,21 @@ const setImages = (images) => {
         </li>`
     ).join('');
 };
+// search images
+const searchImages = (e) => {
+    if(e.key === 'Enter') {
+        inputValue = e.target.value;
+        console.log(inputValue);
+        imagesBlock.innerHTML = '';
+        // let url = `https://api.unsplash.com/search/photos?query=${inputValue}&page=1&per_page=${perPage}&client_id=${apiKey}`;
+        getImages(`https://api.unsplash.com/search/photos?query=${inputValue}&page=${currentPage}&per_page=${perPage}&client_id=${apiKey}`);
+        // getImages(`https://api.unsplash.com/photos/random?client_id=${apiKey}&count=1`);
+    }
+};
+
 
 //run
-getImages(urlTest);
+getImages(url);
+//load btn event
+loadMoreBtn.addEventListener('click', () => getImages(`https://api.unsplash.com/search/photos?query=${inputValue}&page=${currentPage}&per_page=${perPage}&client_id=${apiKey}`));
+searchInput.addEventListener('keyup', searchImages);
